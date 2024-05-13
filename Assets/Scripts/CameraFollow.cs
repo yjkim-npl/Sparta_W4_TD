776684481489f369e0f672640bc7089007987f27
yjkim-npl <kyj8668@gmail.com>
2024-05-13 17:03:05 +0900

    [Update] Camera movement with player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+
+    private void LateUpdate()
+    {
+        var pos = player.transform.position;
+        pos.z = -10;
+        transform.position = pos;
+    }
+}
